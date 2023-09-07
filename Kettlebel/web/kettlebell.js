751c26dc40d85ba1e1cfc854f14b1c70bbd67992
bajnokmilan <bajnok.milan2007@gmail.com>
2023-09-07 16:06:20 +0200

    változtatások

--- Kettlebel/web/kettlebell.js
+++ Kettlebel/web/kettlebell.js
@@ -480,3 +480,32 @@ function gyakorlatGeneralas()
     parallaxAmount: -0.2,
     animateSections: true
   })
+  $(function() {
+    $('.material-card > .mc-btn-action').click(function () {
+        var card = $(this).parent('.material-card');
+        var icon = $(this).children('i');
+        icon.addClass('fa-spin-fast');
+
+        if (card.hasClass('mc-active')) {
+            card.removeClass('mc-active');
+
+            window.setTimeout(function() {
+                icon
+                    .removeClass('fa-arrow-left')
+                    .removeClass('fa-spin-fast')
+                    .addClass('fa-bars');
+
+            }, 800);
+        } else {
+            card.addClass('mc-active');
+
+            window.setTimeout(function() {
+                icon
+                    .removeClass('fa-bars')
+                    .removeClass('fa-spin-fast')
+                    .addClass('fa-arrow-left');
+
+            }, 800);
+        }
+    });
+});
